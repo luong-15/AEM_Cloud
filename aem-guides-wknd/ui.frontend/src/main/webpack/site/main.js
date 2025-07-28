6233e155307e1b6cd136f4c4096e35034f8bb1fb
luong-15 <127217015+luong-15@users.noreply.github.com>
2025-07-28 20:45:20 +0700

    update

--- aem-guides-wknd/ui.frontend/src/main/webpack/site/main.js
+++ aem-guides-wknd/ui.frontend/src/main/webpack/site/main.js
@@ -10,3 +10,5 @@ import '../components/navigation/navigation.js';
 import '../components/form/sign-in-buttons/sign-in-buttons.js';
 import '../components/form/sign-in-form/sign-in-form.js';
 import '../components/contentfragment/js/contributor.js';
+// import '../components/contentfragment/js/author.js'; // Removed missing import to fix build
+import '../components/videoplaylist/videoplaylist.js';
